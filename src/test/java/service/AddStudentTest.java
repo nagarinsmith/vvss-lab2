d753b6b5a5e09ccc3103eed84da5fa5075782503
Paul-Claudiu Orha <paul.orha@p3-group.com>
2020-03-18 11:35:11 +0200

    feat: initial commit, add student test cases and ECTable.pdf

---
+++ src/test/java/service/AddStudentTest.java
@@ -0,0 +1,81 @@
+package service;
+
+import domain.Student;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import repository.StudentXMLRepository;
+import validation.StudentValidator;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AddStudentTest {
+
+    private File studentFile = new File("testStudent.xml");
+    private StudentXMLRepository studentXMLRepository;
+    private Service service;
+
+    @Before
+    public void initialize() {
+        try {
+            studentFile.createNewFile();
+            FileWriter writer = new FileWriter(studentFile);
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<Entitati>\n</Entitati>\n");
+            writer.close();
+        } catch (IOException e) {
+        }
+        studentXMLRepository = new StudentXMLRepository(new StudentValidator(), "testStudent.xml");
+        service = new Service(studentXMLRepository, null, null);
+    }
+
+    @After
+    public void tearDown() {
+        service = null;
+        studentXMLRepository = null;
+        studentFile.delete();
+    }
+
+    @Test
+    public void SaveStudent_ValidData_UserAddedToRepo() {
+        // arrange
+        String id = "5";
+        String name = "TestStudent";
+        int group = 111;
+
+        // act
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student != null;
+        assert student.getNume().equals(name);
+        assert student.getGrupa() == group;
+    }
+
+    @Test
+    public void SaveStudent_InvalidGroup_UserNotAddedToRepo() {
+        // arrange
+        String id = "5";
+        String name = "TestStudent";
+        int group = 109; // Invalid group
+
+        // act
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student == null;
+    }
+}
