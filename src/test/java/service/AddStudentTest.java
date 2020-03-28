dc3f6d393b092416bedd1dcf21e2b83de276b865
Paul-Claudiu Orha <orha.paul@gmail.com>
2020-03-28 20:27:52 +0200

    feat: Add remaining tests for `addStudent`

--- src/test/java/service/AddStudentTest.java
+++ src/test/java/service/AddStudentTest.java
@@ -78,4 +78,107 @@ public class AddStudentTest {
         }
         assert student == null;
     }
+
+    @Test
+    public void SaveStudent_InvalidName_UserNotAddedToRepo() {
+        // arrange
+        String id = "5";
+        String name = "";
+        int group = 111;
+
+        // act
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student == null;
+    }
+
+    @Test
+    public void SaveStudent_InvalidId_UserNotAddedToRepo() {
+        // arrange
+        String id = "";
+        String name = "TestStudent";
+        int group = 111;
+
+        // act
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student == null;
+    }
+
+    @Test
+    public void SaveStudent_NullName_UserNotAddedToRepo() {
+        // arrange
+        String id = "5";
+        String name = null;
+        int group = 111;
+
+        // act
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student == null;
+    }
+
+    @Test
+    public void SaveStudent_NullId_UserNotAddedToRepo() {
+        // arrange
+        String id = null;
+        String name = "TestStudent";
+        int group = 111;
+
+        // act
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student == null;
+    }
+
+    @Test
+    public void SaveStudent_ValidDataAddTwice_UserAddedToRepo() {
+        // arrange
+        String id = "5";
+        String name = "TestStudent";
+        int group = 111;
+
+        // act
+        service.saveStudent(id, name, group);
+        service.saveStudent(id, name, group);
+
+        // assert
+        Student student = null;
+        for (Student stud : service.findAllStudents()) {
+            if (stud.getID().equals(id)) {
+                student = stud;
+            }
+        }
+        assert student != null;
+        assert student.getNume().equals(name);
+        assert student.getGrupa() == group;
+    }
 }
